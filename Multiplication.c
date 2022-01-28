3de51257026290152e34332d524e137ac5f0fd25
Bhavani <98316961+21mh1a04n4@users.noreply.github.com>
2022-01-28 15:09:36 +0530

    //you just need to take two numbers as input from stdin and you need to multiply them #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int product;     product=a*b;     printf("%d",product);     return 0; }

---
+++ Multiplication.c
@@ -0,0 +1,11 @@
+//you just need to take two numbers as input from stdin and you need to multiply them
+#include<stdio.h> 
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int product;
+    product=a*b;
+    printf("%d",product);
+    return 0;
+}
