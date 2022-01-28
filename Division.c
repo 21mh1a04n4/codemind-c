2c14da841257d0151008469c1ed3bdd90d52160f
Bhavani <98316961+21mh1a04n4@users.noreply.github.com>
2022-01-28 15:14:50 +0530

    //you just need to take two numbers as input from stdin and you need to divide them #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int div;     div=a/b;     printf("%d",div);     return 0; }

---
+++ Division.c
@@ -0,0 +1,11 @@
+//you just need to take two numbers as input from stdin and you need to divide them 
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int div;
+    div=a/b;
+    printf("%d",div);
+    return 0;
+}
