65339b5b875dea32f02dfda60b43f8031f0eec5f
Bhavani <98316961+21mh1a04n4@users.noreply.github.com>
2022-01-28 15:01:06 +0530

    //write a program to find the average of two numbers #include<stdio.h> int main() {     float m,n,sum=0,average=0;     scanf("%f%f",&m,&n);     sum=m+n;     average=sum/2;     printf("%0.4f",average); }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,10 @@
+//write a program to find the average of two numbers
+#include<stdio.h>
+int main()
+{
+    float m,n,sum=0,average=0;
+    scanf("%f%f",&m,&n);
+    sum=m+n;
+    average=sum/2;
+    printf("%0.4f",average);
+}
