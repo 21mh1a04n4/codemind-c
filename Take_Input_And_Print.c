df73c6e52f01283da0ce0befde64c179135bb0e6
Bhavani <98316961+21mh1a04n4@users.noreply.github.com>
2022-01-28 15:25:55 +0530

    // take input and output // you just need to take input from stdin and print it to the stdout. #include<stdio.h> int main() {     char str[20];     scanf("%[^ ]s",str);     printf("%s",str);     return 0; }

---
+++ Take_Input_And_Print.c
@@ -0,0 +1,11 @@
+// take input and output
+// you just need to take input from stdin and print it to the stdout.
+#include<stdio.h>
+int main()
+{
+    char str[20];
+    scanf("%[^
+]s",str);
+    printf("%s",str);
+    return 0;
+}
