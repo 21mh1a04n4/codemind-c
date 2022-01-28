b7bc83cc61292d591756f4c45776e1ef7e513817
Bhavani <98316961+21mh1a04n4@users.noreply.github.com>
2022-01-28 15:20:12 +0530

    //modulus //you just need to take two numbers as input from stdin and you need to find modulus of them. #include <stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int mod=a%b;     printf("%d",mod);     return 0; }

---
+++ Modulus.c
@@ -0,0 +1,11 @@
+//modulus
+//you just need to take two numbers as input from stdin and you need to find modulus of them.
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int mod=a%b;
+    printf("%d",mod);
+    return 0;
+}
